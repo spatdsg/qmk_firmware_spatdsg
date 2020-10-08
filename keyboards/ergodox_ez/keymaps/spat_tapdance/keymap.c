86b22acb7e1c83db4ec5889362fd3c571eb08b89
Steve Patrick <spat@microsoft.com>
2020-10-07 18:29:21 -0700

    moved to QWERTY , copied from https://configure.ergodox-ez.com/ergodox-ez/layouts/QajeB/latest/0

--- keyboards/ergodox_ez/keymaps/spat_tapdance/keymap.c
+++ keyboards/ergodox_ez/keymaps/spat_tapdance/keymap.c
@@ -121,15 +121,29 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_ergodox_pretty(
+/*  [0] = LAYOUT_ergodox_pretty(
     KC_TAB,         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           TG(1),                                          KC_PGDOWN,      KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,
     KC_DELETE,      KC_Q,           KC_W,           KC_F,           KC_P,           KC_B,           KC_LBRACKET,                                    KC_RBRACKET,    KC_J,           KC_L,           KC_U,           KC_Y,           TD(SEMICOLON_PRESSANDHOLD_COLON),      KC_BSLASH,
-    KC_BSPACE,      KC_A,           KC_R,           KC_S,           KC_T,           KC_G,                                                                           KC_K,           KC_N,           KC_E,           KC_O,           KC_I,           KC_QUOTE,
+    KC_BSPACE,      KC_A,           KC_R,           KC_S,           KC_T,           KC_G,                                                                           KC_K,           KC_N,           KC_E,           KC_I,           KC_O,           KC_QUOTE,
     KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_D,           KC_V,           KC_LALT,                                        TG(1),          KC_M,           KC_H,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_EQUAL,
     KC_GRAVE,       WEBUSB_PAIR,    KC_TRANSPARENT, KC_LEFT,        KC_RIGHT,                                                                                                       KC_UP,          KC_DOWN,        KC_LBRACKET,    KC_RBRACKET,    KC_CAPSLOCK,
-                                                                                                    KC_LGUI,        TD(X_KC_HOME),        KC_LALT,        TT(2),
+                                                                                                    KC_LGUI,        TD(X_KC_HOME),        KC_LCTRL,        KC_LSHIFT,
                                                                                                                     TD(X_KC_END),         KC_PGUP,
                                                                                     LCTL_T(KC_ESCAPE),KC_TAB,       KC_BSPACE,         KC_PGDOWN,      KC_ENTER,       KC_SPACE
+  ),
+  */
+
+// 10-7-2020 update to match layer 1 for qwerty here:
+
+    [0] = LAYOUT_ergodox_pretty(
+    KC_FIND,        KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_LGUI,                                        KC_INSERT,      KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           SCMD_T(KC_MY_COMPUTER),
+    KC_ESCAPE,      KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_EQUAL,                                       KC_MINUS,       KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
+    KC_BSPACE,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
+    KC_LSPO,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           OSM(MOD_LALT),                                  KC_DELETE,      KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSPC,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_TRANSPARENT,
+                                                                                                    KC_TRANSPARENT, TG(3),          TG(4),          TG(5),
+                                                                                                                    KC_TRANSPARENT, KC_CAPSLOCK,
+                                                                                    RCTL_T(KC_SPACE),LT(2,KC_TAB),   KC_TRANSPARENT, KC_TRANSPARENT, LT(1,KC_ENTER), LSFT_T(KC_SPACE)
   ),
   [1] = LAYOUT_ergodox_pretty(
     KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
